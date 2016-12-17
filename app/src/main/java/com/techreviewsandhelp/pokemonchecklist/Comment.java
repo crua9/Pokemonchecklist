4e60f17675dd4668c0dab725a001d15d7d7d7a8d
Craig Bennett II <craigbennettii@techreviewsandhelp.com>
2016-12-17 13:33:31 -0500

    Added a comment section
    It turns out I can't use images of the actual Pokemon due to copyright
    reasons.
    Anyone reading this, if you have a solution to this, then I will listen.
    
    I'm thinking of having the main checklist shorted by game type or
    generation. That away people
    can just select the check boxed from a dropdown menu.
    
    I'm thinking once the checklist and dropbox sync gets done. I will just
    release the app. But I
    will keep asking for help to improve and add extra features into it.

---
+++ app/src/main/java/com/techreviewsandhelp/pokemonchecklist/Comment.java
@@ -0,0 +1,24 @@
+package com.techreviewsandhelp.pokemonchecklist;
+
+/**
+ * Created by caran on 12/17/2016.
+ */
+
+public class Comment {
+
+    /**
+     The entire point of this class is to leave comments.
+
+         It turns out I can't use images of the actual Pokemon due to copyright reasons.
+     Anyone reading this, if you have a solution to this, then I will listen.
+
+     I'm thinking of having the main checklist shorted by game type or generation. That away people
+     can just select the check boxed from a dropdown menu.
+
+     I'm thinking once the checklist and dropbox sync gets done. I will just release the app. But I
+     will keep asking for help to improve and add extra features into it.
+
+
+
+     */
+}
